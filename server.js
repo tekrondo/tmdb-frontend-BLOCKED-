dc88febff6bb24dbae1f4b11877c0befb7561d95
tekrondo <7498753+tekrondo@users.noreply.github.com>
2018-10-09 11:50:45 +0100

    HEROKU issues

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/tmdbfrontend'));
+app.use(express.static(__dirname + '/dist/tmdb-frontend'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + '/dist/tmdbfrontend/index.html'));
+res.sendFile(path.join(__dirname + '/dist/tmdb-frontend/index.html'));
 
 });
 
