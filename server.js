ca47bf931c7c8f871bed30c8f24595ce4080147e
tekrondo <7498753+tekrondo@users.noreply.github.com>
2018-10-09 11:59:10 +0100

    Heroku

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/tmdb-frontend'));
+app.use(express.static('/dist/tmdb-frontend'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + '/dist/tmdb-frontend/index.html'));
+res.sendFile('/dist/tmdb-frontend/index.html');
 
 });
 
@@ -12,6 +12,6 @@ app.listen(process.env.PORT || 4200);
 
 
 app.get('*', function (req, res) {
-  const index = path.join(__dirname, 'build', 'index.html');
+  const index = path.join(__dirname, 'dist/tmdb-frontend', 'index.html');
   res.sendFile(index);
 });
