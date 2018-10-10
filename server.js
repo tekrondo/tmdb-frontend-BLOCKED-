d8e80f3f93af7fb4049c8120c47d015c9034633d
tekrondo <7498753+tekrondo@users.noreply.github.com>
2018-10-10 21:57:09 +0100

    Updates to deploy to Heroku following this Medium post https://medium.com/@hellotunmbi/how-to-deploy-angular-application-to-heroku-1d56e09c5147

--- server.js
+++ server.js
@@ -4,14 +4,8 @@ const app = express();
 
 app.use(express.static('/dist/tmdb-frontend'));
 app.get('/*', function(req,res) {
-res.sendFile('/dist/tmdb-frontend/index.html');
 
+  res.sendFile('/dist/tmdb-frontend/index.html');
 });
 
 app.listen(process.env.PORT || 4200);
-
-
-app.get('*', function (req, res) {
-  const index = path.join(__dirname, 'dist/tmdb-frontend', 'index.html');
-  res.sendFile(index);
-});
