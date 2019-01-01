a89b2a818ca50c717363933f276d9a5213d2ad93
tekrondo <7498753+tekrondo@users.noreply.github.com>
2019-01-01 11:37:57 +0100

    Worked on the Single movie URL being loaded from the single movie page
    When a single movie is clicked from the Main display, the movie is loaded by way of ngOnInit
    
    This causes a problem because other movies loaded from the similar movies are not displayed or called.
    
    Hacked my way around it by playing with the route event listeners and parsing the URL
    
    WOrks for now

--- src/app/single-movie/single-movie.component.ts
+++ src/app/single-movie/single-movie.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
+
 
 import { MoviesService } from '../services/movies.service';
 import { ActivatedRoute } from '@angular/router'
+
 
 @Component({
   selector: 'app-single-movie',
@@ -16,18 +19,51 @@ export class SingleMovieComponent implements OnInit {
   constructor(
     private moviesService: MoviesService,
     private route: ActivatedRoute,
+    private router: Router
   ) {
     this.movie = {};
     this.similarMovies = [];
+
+    // This section is to handle the loading of a single movie from the similar movies displayed
+    // The way the component is initially designed, it loads the single movie from the main display
+    // on Init, but does not load the single movie while in the single Movie component display
+    // These event handlers take the routes and parse it to get the movie Id and then call the 
+    // getSingleMovie function.
+    // This is a hack and a more suitable way would be researched to solve for this.
+
+    router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) {
+        var routerUrl = event.url
+        var movieId:number  = parseInt(routerUrl.substr(routerUrl.lastIndexOf('/') + 1), 10);
+        this.noMovies = true;
+        this.getSingleMovie(movieId)
+      }
+
+      if (event instanceof NavigationEnd) {
+        this.noMovies = false;
+        window.scrollTo(0, 0)
+      }
+
+      if (event instanceof NavigationError) {
+          console.log(event.error);
+      }
+  });
   }
 
   ngOnInit() {
     const { id } = this.route.snapshot.params;
     this.getSingleMovie(parseInt(id, 10));
-    console.log(id);
+    console.log(id, "ngOnInit");
+
+  //   this.router.events.subscribe((evt) => {
+  //     if (!(evt instanceof NavigationEnd)) {
+  //         return;
+  //     }
+  //     window.scrollTo(0, 0)
+  // });
   }
 
-  getSingleMovie(movieId) {
+  getSingleMovie(movieId: number) {
     this.movie = '';
     this.moviesService.getOneMovie(movieId)
     .subscribe((movie) => {
