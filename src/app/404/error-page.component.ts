a8e77cac927ab2ff90ef8154d67cd107f658d010
tekrondo <7498753+tekrondo@users.noreply.github.com>
2019-01-01 09:37:35 +0100

    Added the image to be full screen
    Made unavailable URLs to load the 404 component

--- src/app/404/error-page.component.ts
+++ src/app/404/error-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-  template: `<h1>THIS PAGE HASN'T BEEN CREATED YET, HOLD ON, YOU MAY HAVE GRAND KIDS BEFORE IT MAY BE ADDED</h1>`
+  template: `<h1 style="margin: 25% auto !important">THIS PAGE HASN'T BEEN CREATED YET, HOLD ON, YOU MAY HAVE GRAND KIDS BEFORE IT MAY BE ADDED</h1>`
 })
 
 export class ErrorPageComponent {
